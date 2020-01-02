f7e79ff240a7607906afd30844a2f3b2c50804d2
Only8Bytes <38161381+Only8Bytes@users.noreply.github.com>
2020-01-02 00:52:38 -0500

    Add files via upload

---
+++ Sort.py
@@ -0,0 +1,12 @@
+a = [10, 20, 70, 90, 60, 30, 40]
+
+for i in range(len(a)):
+    low = i
+    for v in range(i + 1, len(a)):
+        if a[v] < a[low]:
+            low = v
+    a[i], a[low] = a[low], a[i]
+
+print(a)
+
+input()
---
+++ quiz.py
@@ -0,0 +1,18 @@
+def sumOfNum(nums):
+    x = 0
+    def GetVal(number):
+        try:
+            int(number)
+            if int(number) % 2 == 1:
+                return int(number)**2
+            return 0
+        except:
+            return 0
+    if type(nums) is list:
+        for n in nums:
+           x += GetVal(n)
+        return x
+    else:
+        return "Invalid input"
+print(sumOfNum([1,5,-3,5,9,8,4]))
+input()
